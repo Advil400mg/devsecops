e7eafbafa40340b8a9f20fce500646dee86d22f9
Advil400mg <tanguy.vienot.pers@gmail.com>
2026-04-01 19:41:25 +0000

    app + pipeline

---
+++ app/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+import sqlite3
+
+app = Flask(__name__)
+
+# ❌ Hardcoded secret (volontairement vulnérable)
+SECRET_KEY = "supersecret123"
+
+def get_db():
+    return sqlite3.connect("users.db")
+
+@app.route("/")
+def home():
+    return "DevSecOps Lab - Vulnerable App"
+
+# ❌ SQL Injection
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    conn = get_db()
+    cursor = conn.cursor()
+
+    query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
+    cursor.execute(query)
+
+    user = cursor.fetchone()
+    conn.close()
+
+    if user:
+        return "Login successful"
+    else:
+        return "Login failed"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
